7d31a39a89faeb8cb81dd045e0a824988761b570
SocksNFlops <socks&flops@prl.one>
2025-03-06 19:35:44 -0500

    feat(contracts): creating contracts package

---
+++ packages/contracts/src/WethPlusPlus.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+
+contract WethPlusPlus is ERC20Upgradeable {
+    function initialize() public initializer {
+        __ERC20_init("WethPlusPlus", "WPP");
+    }
+}
