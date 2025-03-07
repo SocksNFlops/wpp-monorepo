189ac82dbf046004afc9ca9e78c97eedcd6932b1
SocksNFlops <socks&flops@prl.one>
2025-03-06 19:37:39 -0500

    lint(contracts): fixing tab-width

--- packages/contracts/src/WethPlusPlus.sol
+++ packages/contracts/src/WethPlusPlus.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.28;
 import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
 contract WethPlusPlus is ERC20Upgradeable {
-    function initialize() public initializer {
-        __ERC20_init("WethPlusPlus", "WPP");
-    }
+  function initialize() public initializer {
+    __ERC20_init("WethPlusPlus", "WPP");
+  }
 }
